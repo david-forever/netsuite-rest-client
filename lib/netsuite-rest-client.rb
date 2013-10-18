1595ba69e5e67b3503befcb79688b54f40ffa3b8
David Watson <dwatson@forever.com>
2013-10-18 14:12:55 -0400

    Debug binding.pry

--- lib/netsuite-rest-client.rb
+++ lib/netsuite-rest-client.rb
@@ -208,6 +208,7 @@ module Netsuite
       end
 
       begin
+        binding.pry
         parsed = JSON.parse(reply, :symbolize_names=>true)
       rescue Exception => e
         raise "Unable to parse reply from Netsuite: #{reply}"
